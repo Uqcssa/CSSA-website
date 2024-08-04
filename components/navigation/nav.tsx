0d9c80798ce8348e88d610a8190d94b3b423f2aa
Yusuf ma <13893491334mys@gmail.com>
2024-08-04 21:22:10 +1000

    Finish and fix the bug for the signOut

--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -1,5 +1,22 @@
 import { auth } from "@/server/auth"
+import { UserButton } from "./user-button"
 
 export default async function Nav(){
+    const session = await auth()
     
+    if(session)
+    return(
+        <header className=" bg-slate-500  py-12">
+            <nav>
+                <ul className="flex justify-between">
+                    <li>
+                        Logo
+                    </li>
+                    <li>
+                        <UserButton  expires={session.expires} user={session.user}/>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
