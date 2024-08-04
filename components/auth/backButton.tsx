2e7103e32b7431289b59df8e15e52c424f3c62aa
Yusuf ma <13893491334mys@gmail.com>
2024-08-05 03:53:27 +1000

    set color of the button

--- components/auth/backButton.tsx
+++ components/auth/backButton.tsx
@@ -12,7 +12,7 @@ export const BackButton = ({
     label: string
 }) => {
     return(
-        <Button className="font-medium w-full text-black">
+        <Button className="font-medium w-full text-white bg-purple-600">
             <Link aria-label={label} href={href}>
                 {label}
             </Link>
